90548e6e4bba4a73dcef877918a91fb63b092b10
angelRep <angel_ang_elina@live.com>
2020-11-26 02:08:45 +0200

    More files

--- Lecture05/app.js
+++ Lecture05/app.js
@@ -3,8 +3,11 @@
 
 angular.module('myFirstApp', [])
 
-.controller('MyFirstController', function () {
-
+.controller('MyFirstController', function ($scope) {
+  $scope.name = "Angel";
+  $scope.sayHello = function () {
+    return "Hello Coursera";
+  };
 });
 
 })();
